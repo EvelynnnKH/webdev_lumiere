0a17a0b2d884d803118cb65f43a4343bad9cc7d9
EvelynnnKH <152761307+EvelynnnKH@users.noreply.github.com>
2025-05-31 20:00:37 +0700

    Fix bug gtw apaan pokoke gt 😭

--- database/migrations/2025_05_06_145622_create_carts_table.php
+++ database/migrations/2025_05_06_145622_create_carts_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id('cart_id');
             $table->foreignId('user_id')->constrained('users', 'user_id')->onDelete('cascade');
             $table->boolean('status_del')->default(false);
-            $table->timestamps();
         });
     }
 
