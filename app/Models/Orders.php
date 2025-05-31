648a8b628bdad3446ee512a85cc386923ea25ed3
Brantmarvelsantosa <162331112+Brantmarvelsantosa@users.noreply.github.com>
2025-06-01 01:18:46 +0700

    Update Orders.php

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -5,22 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Orders extends Model
 {
-    //
-    protected $primaryKey = 'order_id';
+    use HasFactory;
 
+    protected $primaryKey = 'order_id';
+    protected $fillable = [
+        'user_id',
+        'order_date',
+        'status',
+        'total_price',
+        'shipping_address',
+        'status_del'
+    ];
+
+    protected $casts = [
+        'order_date' => 'date',
+        'status_del' => 'boolean'
+    ];
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
     }
 
 
-    public function orderItems()
+   public function orderItems()
     {
-        return $this->hasMany(OrderItem::class, 'order_id');
+        return $this->hasMany(OrderItem::class, 'order_id', 'order_id');
     }
 
 
