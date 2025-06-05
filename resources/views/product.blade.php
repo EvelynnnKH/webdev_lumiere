61424a852eff7216b31bc75d98997a87535a8260
MicheelleJeanneth <161410168+MicheelleJeanneth@users.noreply.github.com>
2025-06-05 13:43:28 +0700

    update product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -53,7 +53,7 @@
                         <img src="{{ asset('productimages/'.$p->image_url) }}" alt="{{ $p->name }}">
                     </div>
                     <p class="product-category">{{ $p->category->name }}</p>
-                    <p class="product-name">{{ $p->name }}</p>
+                    <p class="product-name" style="font-size: 1.1rem; font-family: 'Playfair Display'">{{ $p->name }}</p>
                     <p class="card-description">{{ $p->description }}</p>
                     <div class="price-rate">
                         <p class="product-price">Rp. {{ number_format($p->price, 0, ',', '.') }},-</p>
