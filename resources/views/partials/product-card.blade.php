3ccf1a9ca6fbf41df0e9dff779b142d33ef0bd09
MicheelleJeanneth <161410168+MicheelleJeanneth@users.noreply.github.com>
2025-06-08 14:33:15 +0700

    update product-card.blade.php

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -2,10 +2,12 @@
     <div class="card-image-wrapper">
         <img src="{{ asset('productimages/'.$p->image_url) }}" alt="{{ $p->name }}">
     </div>
-    <p class="product-category">{{ $p->category->name }}</p>
-    <p class="product-name" style="font-size: 1.1rem; font-family: 'Playfair Display'">{{ $p->name }}</p>
-    <p class="card-description">{{ $p->description }}</p>
-    <div class="price-rate">
-        <p class="product-price">Rp. {{ number_format($p->price, 0, ',', '.') }},-</p>
+    <p class="product-category text-muted mb-1" style="font-style: italic; font-size: 0.9rem;">{{ $p->category->name ?? 'N/A' }}</p> 
+    <h6 class="product-name mb-1 fw-bold" style="font-size: 1.1rem; font-family: 'Playfair Display'">{{ $p->name }}</h6>
+    <p class="card-description text-muted mb-2" style="font-style: italic; font-size: 0.9rem; height: 70px;">
+        {{ $p->description }}
+    </p>
+    <div class="price-rate d-flex justify-content-between" style="font-size: 0.95rem;">
+        <span class="product-price fw-bold">Rp. {{ number_format($p->price, 0, ',', '.') }},-</span>
     </div>
 </a>
